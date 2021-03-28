5ec5dbc8916875b583fd19f711698e653f1a11fc
Mitul Patel <mitulp534@gmail.com>
2021-03-28 16:49:07 +0530

    Generating cat image and applying css to it.
    more detailed use of flexbox and different very important properties like margin, padding, flex-wrap, shadow, justify-content, etc.

---
+++ cat generator/script.js
@@ -0,0 +1,6 @@
+function generateCat(){
+    var image=document.createElement('img');
+    var div= document.getElementById('flex-cat-gen');
+    image.src="C:/Users/mitul/Downloads/nyanpasu.gif";
+    div.appendChild(image);
+}
